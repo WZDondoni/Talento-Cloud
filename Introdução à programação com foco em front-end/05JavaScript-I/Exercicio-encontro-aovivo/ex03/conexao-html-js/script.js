6c647ac7156046b70669f90da9c4a55941ac2145
WZDondoni <adm.watileyzd@hotmail.com>
2023-10-03 08:41:11 -0300

    fim site ESG-rmç img video duplicados

---
+++ Introdução à programação com foco em front-end/05JavaScript-I/Exercicio-encontro-aovivo/ex03/conexao-html-js/script.js
@@ -0,0 +1,26 @@
+console.log("alguma coisa");
+
+const post2 = document.getElementById('post02');
+console.log(post2);
+
+const formulario = document.getElementById('formulario');
+console.log(formulario);
+
+const posts = document.getElementById('posts');
+console.log(posts);
+
+const data = document.getElementsByClassName("post-data");
+console.log(data);
+
+const textos = document.getElementsByClassName("post-texto");
+console.log(textos);
+
+const listaredesocial = document.getElementsByClassName("lista_redes");
+console.log(listaredesocial);
+
+// integrantes :
+// Watiley 
+// Lucas Araújo
+// Itamar Maximiniano
+
+
